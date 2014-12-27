ae34bef9c1e617dfb56096b8a2323d741e6f735c
Jasmin Ibrisimbegovic <jaskobh@hotmail.com>
2014-12-27 17:42:20 +0100

    proc

--- app.js
+++ app.js
@@ -31,52 +31,29 @@ if ('development' == app.get('env')) {
 }
 
 
-// make `process.stdin` begin emitting "keypress" events
-keypress(process.stdin);
-// listen for the "keypress" event
-keypress(process.stdin);
-if (process.stdin.setRawMode)
-    process.stdin.setRawMode(true);
-else
-    require('tty').setRawMode(true);
-process.stdin.on('keypress', function (c, key) {
-    console.log(0, c, key);
-  if (key && key.ctrl && key.name == 'c') {
-        process.stdin.pause();
-  }
-});
-process.stdin.on('mousepress', function (mouse) {
-    console.log(mouse);
-});
-keypress.enableMouse(process.stdout);
-process.on('exit', function () {
-    //disable mouse on exit, so that the state is back to normal
-    //for the terminal.
-    keypress.disableMouse(process.stdout);
-});
-process.stdin.resume();
 app.get('/', routes.index);
 app.get('/about', routes.about);
 app.get('/contact', routes.contact);
 
 app.get('/takeoff', function (req, res) {
+       //res.send("takeoff");
     client.takeoff();
 });
 
 app.get('/land', function (req, res) {
- //   res.send("land");
+    //res.send("land");
     client.stop();
     client.land();
 
 });
 
 app.get('/spinleft', function (req, res) {
- // res.send("spinleft");
+  //res.send("spinleft");
     client.counterClockwise(0.5);
 });
 
 app.get('/spinright', function (req, res) {
-  //  res.send("spinright");
+    //res.send("spinright");
     client.clockwise(0.5);
 });
 
